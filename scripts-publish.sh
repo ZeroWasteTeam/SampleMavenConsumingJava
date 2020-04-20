17ec69fe8e922693de886cb80fc6bf2cdc5181c8
Nachiappan <nachiapan@gmail.com>
2020-04-20 16:54:58 +0530

    Changed the repo name

--- scripts-publish.sh
+++ scripts-publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gradle -Pgpr.repo="https://maven.pkg.github.com/ZeroWasteTeam/SampleJavaMavenPackage" -Pgpr.user="${1}" -Pgpr.key="${2}" -Pgpr.version="${3}" publish
+gradle -Pgpr.repo="https://maven.pkg.github.com/ZeroWasteTeam/SampleMavenConsumingJava" -Pgpr.user="${1}" -Pgpr.key="${2}" -Pgpr.version="${3}" publish
 
